9a537f8af149c7e100b0454bb28367dbc6f1bcda
pjjustdoit <pjlovepy@126.com>
2017-02-23 14:20:52 +0800

    Add files via upload

---
+++ thread/CustomThread.java
@@ -0,0 +1,21 @@
+package com.pingan.qhzx.thread;
+
+/**
+ * Created by peijian280 on 17/2/23.
+ */
+public class CustomThread extends Thread {
+    public CustomThread() {
+        super("[CustomThread] 子线程");
+    }
+
+    @Override
+    public void run() {
+        System.out.println(Thread.currentThread().getName()+": start.");
+        try {
+            Thread.sleep(1000);
+            System.out.println(Thread.currentThread().getName()+": end.");
+        } catch (Exception e) {
+            System.out.println("Exception from " + Thread.currentThread().getName());
+        }
+    }
+}
